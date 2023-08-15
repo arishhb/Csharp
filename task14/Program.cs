7772418b50ce5a06c827db070b7760f4ed759051
Arisha <arisha.batalova14@yandex.ru>
2023-08-15 10:11:39 +0300

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.*/
+ Console.WriteLine("введите число");
+ int number1= Convert.ToInt32(Console.ReadLine());
+ if (number1%23==0 && number1%7==0){
+    Console.WriteLine("кратно 23 и 7");
+ }
+ else {
+    Console.WriteLine("некратно 23 и 7");
+ }
