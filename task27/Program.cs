3ec6e39104198bee2823962b5953052b1c3c5245
Arisha <arisha.batalova14@yandex.ru>
2023-09-20 20:26:03 +0300

    27

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+int result= numbersum(number);
+Console.WriteLine(result);
+
+int numbersum(int num){
+    int sum=0;
+    while(num>9){
+        sum=num%10+sum;
+        num=num/10;
+    }
+    sum=sum+num;
+    return sum;
+}
